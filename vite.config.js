de8d6a0bea1a46664cd983a2122f31abf023e6e6
fabulouzsage <fabuloussage@yahoo.com>
2024-01-20 09:51:13 +0100

    DAO

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      'events': 'events', // Alias 'events' to the installed package
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        nodeResolve({
+          browser: true,
+          preferBuiltins: false,
+        }),
+        commonjs(),
+      ],
+    },
+  },
+});
